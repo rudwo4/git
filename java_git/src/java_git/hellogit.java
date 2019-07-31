bbf6a4d442e19e4bf8f4b37255dd46427decd613
ksm17 <ksm17@DESKTOP-E55HMPK>
2019-07-31 15:10:00 +0900

    테스트

--- java_git/src/java_git/hellogit.java
+++ java_git/src/java_git/hellogit.java
@@ -2,6 +2,6 @@ package java_git;
 
 public class hellogit {
 	public static void main(String[] args) {
-		System.out.println("hellogit");
+		System.out.println("hello git");
 	}
 }
---
+++ java_git/src/java_git/testgit.java
@@ -0,0 +1,5 @@
+package java_git;
+
+public class testgit {
+
+}
